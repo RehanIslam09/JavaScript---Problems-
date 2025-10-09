e205dfa2ffebf601c779ebdc5c39d0cfa661862b
RehanIslam09 <rehanislamcontact@gmail.com>
2025-10-09 18:07:06 +0530

    longest word in a string

--- Objects & Arrays in JavaScript/SST - Assignments/Assignment 05.js
+++ Objects & Arrays in JavaScript/SST - Assignments/Assignment 05.js
@@ -1,8 +1,36 @@
 // =========================================
 /*
     Question : 
+      Write a function that takes in a string and returns the longest word in that string.
+      If there are multiple words with the same length, return the first one you encounter.
 
+      Example:
+
+
+      Input:
+
+      "JavaScript is a fun programming language"
+
+
+      Output:
+
+      "programming"
 */
 // =========================================
 
+const str = 'JavaScript is a fun programming language';
 
+function findLongestWord(str) {
+  // Write your code here
+  // return the result
+  const words = str.split(' ');
+  let longestWord = '';
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) longestWord = words[i];
+  }
+
+  return longestWord;
+}
+
+console.log(findLongestWord(str));
